99379bfd90c0b0344a50cf1c8c2db0d9b6249ed0
chikaah <oluebubesomto9@gmail.com>
2025-05-10 00:57:25 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,29 @@
+let isWaiting = false;
+
+function startEarning() {
+  if (isWaiting) return;
+
+  isWaiting = true;
+  let timeLeft = 10;
+  document.getElementById("countdown").innerText = `Time: ${timeLeft}s`;
+  document.getElementById("reward").innerText = "";
+
+  // Load Adsterra smart link
+  const adLinks = [
+    "https://www.profitableratecpm.com/gek6e26ws?key=8fc5fec2b6aa16de54e32b8a1a75c315"
+  ];
+  const randomAd = adLinks[Math.floor(Math.random() * adLinks.length)];
+  document.getElementById("adFrame").src = randomAd;
+
+  const countdownInterval = setInterval(() => {
+    timeLeft--;
+    if (timeLeft > 0) {
+      document.getElementById("countdown").innerText = `Time: ${timeLeft}s`;
+    } else {
+      clearInterval(countdownInterval);
+      document.getElementById("countdown").innerText = "Done!";
+      document.getElementById("reward").innerText = "You earned $0.0001";
+      isWaiting = false;
+    }
+  }, 1000);
+}￼Enter
